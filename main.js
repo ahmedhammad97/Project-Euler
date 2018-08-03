65b253bc17ea53a65c24124a0b2b47130ff68c9e
Ahmed Hammad <hammad97official@gmail.com>
2018-08-03 15:01:58 +0200

    Problem-3 solved

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let sum = 2;
-let x=1,y=2;
-while(y<4000000){
-  let z = x+y;
-  x = y;
-  y = z;
-  if(z%2===0){sum+=z;}
+let input = 600851475143;
+for(i=2; i<input; i++){
+  if(input%i === 0 && isPrime(input/i)){console.log(input/i); break;}
 }
-console.log(sum);
+
+
+
+function isPrime (num){
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false;
+    return num !== 1;
+}
