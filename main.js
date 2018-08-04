6432162df015a3024e348345fd602ebf5d59b730
Ahmed Hammad <hammad97official@gmail.com>
2018-08-04 17:40:19 +0200

    problem-7 solved

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-let sum=0, sumSquare=0;
-for(i=1; i<=100; i++){
-  sum+=i;
-  sumSquare+=(Math.pow(i,2));
+let rank = process.argv.slice(2)[0];
+
+let counter=1, primeCounter=0;
+while(true){
+  if(isPrime(counter)){
+    if(primeCounter==rank-1){console.log(counter); break;}
+    primeCounter++;
+  }
+  counter++;
 }
-console.log(Math.pow(sum,2) - sumSquare);
+
+function isPrime (num){
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false;
+    return num !== 1;
+}
